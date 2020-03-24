50e5128cdf1091e4f03a2c70e1108643858703f9
xiaodaxia1083394272 <1083394272@qq.com>
2020-03-24 20:45:47 +0800

    new

---
+++ eleme/src/components/HeaderTop.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <slot name="left"></slot>
+    <span class="header_title">
+    </span>
+    <slot name="right"></slot>
+  </header>
+</template>
+
+<script>
+export default {
+    
+
+}
+</script>
+
+
